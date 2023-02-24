1940886922287a780f5212346f84937555af1309
Tóth Csaba <csaba_toth@hotmail.com>
2023-02-24 10:06:24 +0100

    shares the 02.23 lesson - Calculator.

---
+++ src/test/java/TestCalc.java
@@ -0,0 +1,39 @@
+import io.qameta.allure.*;
+import org.example.Calculator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+
+@Epic("Epic 1")
+public class TestCalc {
+
+
+    @Test
+    @Story("Story 1")
+    @Description("Description related to testAdd test")
+    @Severity(SeverityLevel.NORMAL)
+    @Tag("Tag 1")
+    @Link("https://example.org")
+    public void testAdd() {
+        Calculator calc = new Calculator();
+        Assertions.assertEquals(7, calc.Add(4, 3));
+
+    }
+
+    private Calculator createCalc() {
+        return new Calculator();
+    }
+
+    @Test
+    @Story("User Story2")
+    @Description("Description related to TestAddWithNegative")
+    @Severity(SeverityLevel.BLOCKER)
+    @Tag("Tag 2")
+    public void testAddWithNegative() {
+        Calculator calc = new Calculator();
+        Assertions.assertEquals(-5, calc.Add(-2, -3) );
+    }
+
+
+}
